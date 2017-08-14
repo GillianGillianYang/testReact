c88a64d16293b5460ba6c32e0cc30e0d5375ab5b
MrRabit <Gillian@GilliantekiMacBook-Air-2.local>
2017-08-14 22:06:05 +0800

    react 00

--- client/app.jsx
+++ client/app.jsx
@@ -5,10 +5,47 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+class PlaceItem extends React.Component{
+
+
+  render(){
+    return(
+        <div className="item">
+          <i className="large youtube middle aligned icon"></i>
+          <div className="content">
+            <a className="header">{this.props.place}</a>
+          </div>
+        </div>
+    ); 
+  }
+
+}
+
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello, world!</h1>
+      <div className="ui middle aligned center aligned grid">
+    <div className="fourteen wide column">
+      <h2 className="ui teal image header">
+        <i className="hand spock icon"></i>
+        <div className="content">
+          Search the place
+        </div>
+      </h2>
+      <div className="ui action input">
+        <input id="urlText" type="text" placeholder="Search..."/>
+        <button id="searchButton" className="ui blue button">Search</button>
+      </div>
+      <div className="ui horizontal divider">
+        place-spot
+      </div>
+      <div id="resultList" className="ui relaxed divided list">
+        <PlaceItem place="taipei 101"/>
+        <PlaceItem place="NTU"/>
+        <PlaceItem place="market"/>
+      </div>
+    </div>
+  </div>
     )
   }
 }
